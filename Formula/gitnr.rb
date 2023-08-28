a4ad7104f11f38b48fe2aeb3f2814fe334b1c1c0
tarun <7835503+realfresh@users.noreply.github.com>
2023-08-28 09:57:14 +0800

    fix: gitnr formula

--- Formula/gitnr.rb
+++ Formula/gitnr.rb
@@ -1,5 +1,5 @@
 class Gitnr < Formula
-  desc "A CLI utility to create .gitignore files using one or more templates"
+  desc "CLI utility to create .gitignore files using one or more templates"
   homepage "https://github.com/reemus-dev/gitnr"
   url "https://github.com/reemus-dev/gitnr/archive/refs/tags/v0.1.37.tar.gz"
   sha256 "0bf57f504193705412f1a513c84fe458c4336243e24a5ed8b1e609e90fc2ebb0"
@@ -8,20 +8,10 @@ class Gitnr < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gitnr`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
